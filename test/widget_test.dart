11e2436acb0916fcdce3cd95d4cb7eb4aaf0a1ef
mahmoudmohamed <mahmoudmohamed01559@gmail.com>
2026-03-23 00:43:45 +0200

    feat: Set up initial Flutter application structure with BLoC for theme and locale management, service locator, and routing.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:notiva/main.dart';
+import 'package:notiva/notiva_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const NotivaApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
